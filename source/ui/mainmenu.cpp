1302cbb4f7849264ff5926b16ebf4b86c7940f40
leo60228 <iakornfeld@gmail.com>
2016-12-22 16:13:09 -0500

    need more help

--- source/ui/mainmenu.cpp
+++ source/ui/mainmenu.cpp
@@ -1,5 +1,12 @@
 #include <malloc.h>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <unistd.h>
+#include <cstring>
 
 #include <3ds.h>
 
@@ -12,35 +19,28 @@ extern "C" {
     #include "../core/screen.h"
 }
 
-struct run_plugin {
-    run_plugin(u64 tid) : tid(tid) {}
-    void operator()() {
-        if (tid == 0) {
-            files_open_pluginfs();
-            return;
-        }
-    }
-
-private:
-    u64 tid;
-};
-
-typedef struct plugin_list_item_s {
-    char name[LIST_ITEM_NAME_MAX];
-    u32 color;
-    run_plugin runner;
-} plugin_list_item;
-
 static list_item pluginfs_entry = {"SD", COLOR_TEXT, (void*)(int)0};
-static plugin_list_item fsplugins[64];
+static std::vector<u64> pluginids;
 
-/*extern "C" {
-    void mainmenu_update(ui_view*, void*, linked_list*, list_item*, bool);
+static void mainmenu_launch_title(u64 tid) {
+    Result res = 0;
 
-    void mainmenu_open();
+    if(R_SUCCEEDED(res = APT_PrepareToDoApplicationJump(0, tid, (u8)3))) {
+        u8 param[0x300];
+        u8 hmac[0x20];
 
-    void mainmenu_draw_top(ui_view*, void*, float, float, float, float, list_item*);
-}*/
+        res = APT_DoApplicationJump(param, sizeof(param), hmac);
+    }
+}
+
+static void mainmenu_run_plugin(u64 pluginid) {
+    if (pluginid == 0) {
+        files_open_pluginfs();
+    } else {
+        mainmenu_launch_title(pluginid);
+    }
+}
+
 static void mainmenu_draw_top(ui_view* view, void* data, float x1, float y1, float x2, float y2, list_item* selected) {
     u32 logoWidth;
     u32 logoHeight;
@@ -51,16 +51,72 @@ static void mainmenu_draw_top(ui_view* view, void* data, float x1, float y1, flo
     screen_draw_texture(TEXTURE_LOGO, logoX, logoY, logoWidth, logoHeight);
 }
 
+static std::vector<std::string> split(const std::string &s, char delim) {
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> tokens;
+    while (getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
+
+static std::string readFile2(const std::string &fileName) {
+    std::ifstream ifs(fileName.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
+
+    std::ifstream::pos_type fileSize = ifs.tellg();
+    ifs.seekg(0, std::ios::beg);
+
+    std::vector<char> bytes(fileSize);
+    ifs.read(&bytes[0], fileSize);
+
+    return std::string(&bytes[0], fileSize);
+}
+
 static void mainmenu_update(ui_view* view, void* data, linked_list* items, list_item* selected, bool selectedTouched) {
     if(selected != NULL && (selectedTouched || hidKeysDown() & KEY_A)) {
-        fsplugins[(int)(selected->data)].runner();
+        mainmenu_run_plugin((u64)(selected->data));
         return;
     }
 
     if(linked_list_size(items) == 0) {
-        plugin_list_item pluginfs = {"SD", COLOR_TEXT, run_plugin((u64)0)};
-        fsplugins[0] = pluginfs;
+        pluginids.push_back(0);
         linked_list_add(items, &pluginfs_entry);
+        if(access("ctrm/plugins.txt", F_OK) != -1) {
+            std::string filename = "ctrm/plugins.txt";
+            
+            std::string pluginsfile = readFile2(filename);
+
+            int i = 1;
+            
+            char* cfile = const_cast<char*>(pluginsfile.c_str());
+
+            char* line = strtok(cfile, "\n");
+            
+            std::ofstream log("ctrmlog.txt");
+
+            while (line != NULL) {
+                std::string stdline = line;
+                
+                log << stdline << "\n";
+
+                std::vector<std::string> plugininfo = split(stdline, ' ');
+                
+                log << plugininfo[0] << "\n";
+                log << plugininfo[1] << "\n";
+
+                list_item plugin_entry;
+                strcpy(plugin_entry.name, plugininfo[1].c_str());
+                plugin_entry.color = COLOR_TEXT;
+                plugin_entry.data = (void*)i;
+
+                linked_list_add(items, &plugin_entry);
+
+                pluginids.push_back(strtoull(plugininfo[0].c_str(), NULL, 10));
+                i++;
+                line = strtok(NULL, "\n");
+            }
+        }
     }
 }
 
